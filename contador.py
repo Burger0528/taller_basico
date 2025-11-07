d932606a0e4683a2a84dddde698e44bcac0eba46
andres <andreshurtado3000@icloud.com>
2025-11-07 13:35:44 -0500

    new add

---
+++ contador.py
@@ -0,0 +1,9 @@
+"""numero=1
+while numero<=10:
+    
+    print(numero)
+    numero += 1
+"""
+#otra opcion
+for i in range(1,10):
+    print(i)
---
+++ sumatoria_n.py
@@ -0,0 +1,4 @@
+contador= 0
+for i in range(1,11):
+    contador+= i
+    print(contador)
