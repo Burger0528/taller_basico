bb7cba648cf0d4aba9bf99f174ac2f3a78f507cf
andres <>
2025-11-10 11:10:57 -0500

    listas

--- lista_futas.py
+++ lista_futas.py
@@ -1,2 +1,19 @@
 frutas=["naranja","pera","manzana"]
 print(frutas)
+frutas.append("banana")
+print(frutas)
+
+#frutas.remove("naranja")
+print(frutas)
+
+
+frutas.insert(1,"tomate")
+print(frutas)
+
+frutas.pop(-1)
+print(frutas)
+
+esta_banana= "pera" in frutas
+print(esta_banana)
+esta_banana=frutas.index("pera")
+print(esta_banana)
---
+++ lista_promedios
@@ -0,0 +1,5 @@
+numeros= [10, 15, 14, 10, 9]
+print(f"lista de numeros {numeros}")
+suma_total = sum(numeros)
+promedio= suma_total / len(numeros)
+print(f"{suma_total} y el promedio de los numeros es {promedio}")
